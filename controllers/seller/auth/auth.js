f48e1aba71b8fc881d4249d8528957220394fc86
Administrator <gitlab_admin_76a543@example.com>
2024-07-28 20:47:58 +0700

    sebelum nambah models command

--- controllers/seller/auth/auth.js
+++ controllers/seller/auth/auth.js
@@ -10,47 +10,48 @@ async function login(req, res) {
     const customer = await Customer.findOne({
       where: { email },
     });
+
+    // Tambahkan pemeriksaan untuk memastikan pelanggan ditemukan
+    if (!customer) {
+      return res.status(401).json({
+        status: 401,
+        message: "Username atau password salah",
+      });
+    }
+
     const passwordHash = await comparePassword(
       password,
       customer.dataValues.password
     );
+
     if (passwordHash) {
-      if (customer) {
-        const payload = {
-          id_customer: customer.id_customer,
-          email: customer.email,
-          verifikasi: customer.verifikasi,
-          nama_lengkap : customer.nama_lengkap
-        };
-        if (customer.verifikasi === "T") {
-          return res
-            .status(400)
-            .json({
-              code: 400,
-              message:
-                "user belum verifikasi, silahkan verifikasi terlebih dahulu",
-            });
-        }
-        const token = jwt.sign(payload, secretKey, { expiresIn: "3d" });
+      const payload = {
+        id_customer: customer.id_customer,
+        email: customer.email,
+        verifikasi: customer.verifikasi,
+        nama_lengkap: customer.nama_lengkap
+      };
 
-        res.cookie("token", token, {
-          httpOnly: true,
-          // secure: process.env.NODE_ENV === 'production',
-          sameSite: "strict",
-          maxAge: 3 * 24 * 60 * 60 * 1000,
-        });
-
-        res.json({
-          // customer,
-          status: 200,
-          message: "Login berhasil",
-        });
-      } else {
-        res.status(401).json({
-          status: 401,
-          message: "Username atau password salah",
+      if (customer.verifikasi === "T") {
+        return res.status(400).json({
+          code: 400,
+          message: "User belum verifikasi, silahkan verifikasi terlebih dahulu",
         });
       }
+
+      const token = jwt.sign(payload, secretKey, { expiresIn: "3d" });
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        sameSite: "strict",
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        status: 200,
+        message: "Login berhasil",
+      });
     } else {
       res.status(401).json({
         status: 401,
