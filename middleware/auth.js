02a5d4d52fed79ccf68450c6cf67210c7f0ced27
Administrator <gitlab_admin_76a543@example.com>
2024-08-01 11:46:08 +0700

    cors

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,17 @@ require("dotenv").config();
 const secretKey = process.env.SECRET_KEY;
 
 function authenticateJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
+  // Ambil token dari cookies
+  const token = req.cookies.token;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
+  if (token) {
     jwt.verify(token, secretKey, (err, user) => {
       if (err) {
         return res.status(403).json({ status: 403, message: 'Token tidak valid' });
       }
 
-      req.user = user;
-      next();
+      req.user = user; // Menyimpan informasi pengguna di request object
+      next(); // Lanjutkan ke middleware berikutnya atau route handler
     });
   } else {
     res.status(401).json({ status: 401, message: 'Autentikasi diperlukan' });
