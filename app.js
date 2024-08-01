ac9c92e130e7375f373645037f937ecff9122b16
Administrator <gitlab_admin_76a543@example.com>
2024-08-01 10:21:09 +0700

    cors

--- app.js
+++ app.js
@@ -24,7 +24,23 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    allowedHeaders:
+      "Content-Type, Authorization, Origin, X-Requested-With, Accept",
+  })
+);
+// app.use(cors());
+app.options("*", cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
